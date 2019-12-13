75aa4ee4db49d4b33440981e6d43d2f8868b444c
ShashiShekharMahto <55286704+ShashiShekharMahto@users.noreply.github.com>
2019-12-13 08:03:38 +0530

    Update staircase_hackerrank.py

--- staircase_hackerrank.py
+++ staircase_hackerrank.py
@@ -1,3 +1,25 @@
+#1st approach
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n):
+        for j in range(n-(i+1)):
+            print(" ",end='')
+        for k in range(i+1):
+            print("#",end='')
+        print()
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
+    
+#2nd approach
+
 import math
 import os
 import random
